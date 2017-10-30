58e08bddbdbbe2868eb1d1f5dbdfb23467f6a285
Mohammed Rahaman <mohammed.rahaman@happiestminds.com>
2017-10-30 15:02:02 +0530

    Initial files

---
+++ db1.sql
@@ -0,0 +1,11 @@
+create database ranga;
+create table Employees (
+        id bigint not null auto_increment,
+        age integer,
+        name varchar(255),
+        salary float,
+        primary key (id)
+)
+
+
+SELECT * FROM Employees
